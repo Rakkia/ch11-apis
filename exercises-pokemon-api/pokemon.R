e67d69f68d3a65d0f0f11e4b0c9e9be62b4711e2
Xiaochu Lin <xiaochulin@D-10-18-130-110.dhcp4.washington.edu>
2018-02-07 15:18:43 -0800

    practice

--- exercises-pokemon-api/pokemon.R
+++ exercises-pokemon-api/pokemon.R
@@ -1,11 +1,27 @@
+
 ### API Practice ###
 
 ## Load the httr and jsonlite libraries for accessing data
+library("httr")
+library("jsonlite")
 
 # What is the name of the first pokemon?
-
+base.url <- "http://pokeapi.co/api/v2/pokemon/1"
+response <- GET(base.url)
+body <- content(response, "text")
+results <- fromJSON(body)
+result <- results$name
 # What is pikachu's ID number?
+base.url <- "http://pokeapi.co/api/v2/pokemon/pikachu"
+response <- GET(base.url)
+body <- content(response, "text")
+results <- fromJSON(body)
+pikachu <- results$id
 
 # What is the size of the "cheri" berry?
-
+base.url <-"http://pokeapi.co/api/v2/berry/cheri"
+response <- GET(base.url)
+body <- content(response, "text")
+results <- fromJSON(body)
+cheri <- results$size
 # How many pokemon are in the cave habitat?
